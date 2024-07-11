ee114b69f2416907b20fe66bd552dc72b0d86918
Chanukaisuru <chanukaisuru26@gmail.com>
2024-07-12 00:12:53 +0530

    add js part for start in select_doctor.js

--- resources/js/select_doctor.js
+++ resources/js/select_doctor.js
@@ -13,43 +13,56 @@ document.getElementById('btns').innerHTML = btns.map(btn => {
     return `<button class='fil-p' onclick='filterItems(${id})'>${distric}</button>`;
 }).join('');
 
-const card=[
-    {id:1, image:'resources/img/selectDoctor_image.jpg', Name:'Sampath', distric:'Matar', Specile:'genaral'},
-    {id:3, image:'resources/img/selectDoctor_image.jpg', Name:'Isuru', distric:'colombo', Specile:'genaral'},
-    {id:1, image:'resources/img/selectDoctor_image.jpg', Name:'Kamal', distric:'Matar', Specile:'genaral'},
-    {id:2, image:'resources/img/selectDoctor_image.jpg', Name:'Surith', distric:'Galle', Specile:'genaral'},
-    {id:1, image:'resources/img/selectDoctor_image.jpg', Name:'Sarith', distric:'Matar', Specile:'genaral'},
-    {id:3, image:'resources/img/selectDoctor_image.jpg', Name:'Sampath', distric:'colombo', Specile:'genaral'},
-]
+const card = [
+    { id: 1, image: 'resources/img/selectDoctor_image.jpg', Name: 'Sampath', distric: 'Matar', Specile: 'genaral', hospital: 'Distric Genaral Hospital', rating: 4 },
+    { id: 3, image: 'resources/img/selectDoctor_image.jpg', Name: 'Isuru', distric: 'colombo', Specile: 'genaral', hospital: 'Distric Genaral Hospital', rating: 5 },
+    { id: 1, image: 'resources/img/selectDoctor_image.jpg', Name: 'Kamal', distric: 'Matar', Specile: 'genaral', hospital: 'Distric Genaral Hospital', rating: 3 },
+    { id: 2, image: 'resources/img/selectDoctor_image.jpg', Name: 'Surith', distric: 'Galle', Specile: 'genaral', hospital: 'Distric Genaral Hospital', rating: 4 },
+    { id: 1, image: 'resources/img/selectDoctor_image.jpg', Name: 'Sarith', distric: 'Matar', Specile: 'genaral', hospital: 'Distric Genaral Hospital', rating: 2 },
+    { id: 3, image: 'resources/img/selectDoctor_image.jpg', Name: 'Sampath', distric: 'colombo', Specile: 'genaral', hospital: 'Distric Genaral Hospital', rating: 5 },
+];
 
-const categories= [...new Set(card.map((item)=>
-    {return item}))]
+const categories = [...new Set(card.map(item => { return item }))];
 
-const filterItems=(a)=>{
-    const filterCategories=categories.filter(item);
-    function item(value){
-        if(value.id==a){
-            return(
-                value.id
-            )
+const filterItems = (a) => {
+    const filterCategories = categories.filter(item => item.id == a);
+    displayItem(filterCategories);
+}
+
+const displayItem = (items) => {
+    document.getElementById('root').innerHTML = items.map((item) => {
+        var { image, Name, distric, Specile, hospital, rating } = item;
+        return (
+            `<div class="card">
+                <div class="card-content">
+                    <div class="image">
+                        <img src=${image} alt="doc image">
+                    </div>
+                </div>
+                <div class="name-profession">
+                    <span class="name">${Name}</span>
+                    <span class="specialty">${Specile}</span>
+                    <span class="hospital">${hospital}</span>
+                    <span class="distric">${distric}</span>
+                </div>
+                <div class="star">
+                    ${generateStars(rating)}
+                </div>
+            </div>`
+        )
+    }).join('');
+}
+
+const generateStars = (rating) => {
+    let stars = '';
+    for (let i = 0; i < 5; i++) {
+        if (i < rating) {
+            stars += '<i class="fa-solid fa-star"></i>';
+        } else {
+            stars += '<i class="fa-regular fa-star"></i>';
         }
     }
-    displayItem(filterCategories)
-}    
-
-const displayItem =(items) =>{
-    document.getElementById('root') .innerHTML =items.map((item)=>
-    {
-        var {image,Name,distric,Specile}=item;
-        return(
-            `<div class='box'>
-                <h3>${Name}</h3>
-                <div class='img-box'>
-                    <img class='selectDoctor_image' src=${image}></img>
-                </div>
-                <h2>${distric}</h2>
-                <h2>${Specile}</h2>
-            </div>`)
-    } ).join('');
+    return stars;
 }
-displayItem(categories);      
+
+displayItem(categories);
