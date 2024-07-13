417576d1886c4859576b326e86f91dafd4349fa2
Harshani-530 <harshanimadhushani2001@gmail.com>
2024-07-13 13:30:06 +0530

    Added feedback_view.css & feedback_view.jpg

--- feedback_view.php
+++ feedback_view.php
@@ -14,7 +14,7 @@ $result = $conn->query($sql);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Feedbacks</title>
-    <link rel="stylesheet" href="feedback_view.css">
+    <link rel="stylesheet" href="resources/css/feedback_view.css">
 </head>
 
 <body>
